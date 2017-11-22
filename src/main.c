448db7341d36bda2e6291eed4c30de28484a1b4c
PierrickJACQUETTE <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-11-22 22:22:57 +0100

    main du projet

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "pcap.h"
+
+int main (int argc, char *argv[]){
+  if(argc > 1){
+    pcap(argv[1]);
+  }
+  else{
+    fprintf(stderr, "Merci d'indiquer un fichier en argument\n");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
