4b72510715a2a939b393de54cfb14faa6e082f2f
PierrickJACQUETTE <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-11-23 08:42:53 +0100

    entité telnet_message : permet d'afficher la commande d'un paquet telnet

---
+++ src/telnet_message.c
@@ -0,0 +1,78 @@
+#include "telnet_message.h"
+
+void commande(unsigned char * message, int* messageConnu, int* sb){
+    *sb = 0;
+    switch(*message){
+        case 240:
+            print("Se", *message);
+            break;
+        case 241:
+            print("No operation", *message);
+            break;
+        case 242:
+            print("Data mark", *message);
+            break;
+        case 243:
+            print("Break", *message);
+            break;
+        case 244:
+            print("Interrupt process", *message);
+            break;
+        case 245:
+            print("Abort output", *message);
+            break;
+        case 246:
+            print("Are you there", *message);
+            break;
+        case 247:
+            print("Erase character", *message);
+            break;
+        case 248:
+            print("Erase line", *message);
+            break;
+        case 249:
+            print("Go ahead", *message);
+            break;
+        case 250:
+            print("Sb", *message);
+            *sb = 1;
+            break;
+        case 251:
+            print("Will", *message);
+            break;
+        case 252:
+            print("Won't", *message);
+            break;
+        case 253:
+            print("Do", *message);
+            break;
+        case 254:
+            print("Don't", *message);
+            break;
+        case 255:
+            print("Interpret as command", *message);
+            break;
+        default:
+            *messageConnu=0;
+            printf("%c",*message);
+            break;
+    }
+}
+
+void print_message(unsigned char** message, int* i){
+    int messageConnu = 1, sb = 0, option = 0;
+    commande(&((*message)[*i]), &messageConnu, &sb);
+     *i = *i +1;
+     if(messageConnu == 1){
+         print_option(&((*message)[*i]), i, &option);
+         *i = *i +1;
+         printf("   ");
+         if(sb == 1 && option == 32){
+             if((*message)[*i] == 1){
+                 print("Send", (*message)[*i]);
+                          printf("   ");
+                  *i = *i +1;
+            }
+         }
+     }
+}
---
+++ src/telnet_message.h
@@ -0,0 +1,9 @@
+#ifndef TELNET_MESSAGE_H
+#define TELNET_MESSAGE_H
+
+#include "annexe.h"
+#include "telnet_option.h"
+
+void print_message(unsigned char** message, int* i);
+
+#endif
