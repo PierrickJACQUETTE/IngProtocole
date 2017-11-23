653b9c25e0fdf3a947c2ae8d46fb6cc742334176
PierrickJACQUETTE <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-11-23 08:40:07 +0100

    entité paquet : permet de parser et d'afficher un paquet

---
+++ src/paquet.c
@@ -0,0 +1,66 @@
+#include "paquet.h"
+
+int parsePaquet(const u_char *truepacket, struct paquet** paquet){
+    int parse = 0;
+    (*paquet)->ip=(struct ip*)(truepacket+14);
+    if((*paquet)->ip->ip_p == IPPROTO_TCP){
+        (*paquet)->tcp=(struct tcphdr*)(truepacket+14+(4*(*paquet)->ip->ip_hl));
+        if (((unsigned short int)ntohs((*paquet)->tcp->dest) == 23) || (unsigned short int)ntohs((*paquet)->tcp->source) == 23){
+            struct telnet* t = malloc(sizeof(struct telnet));
+            ERROR_NULL(t, "malloc telnet : parsePaquet : paquet.c");
+            t->message=(unsigned char*)(truepacket+14+(4*(*paquet)->ip->ip_hl)+(*paquet)->tcp->doff*4);
+            t->size_telnet=ntohs((*paquet)->ip->ip_len)-(*paquet)->tcp->doff*4-20;
+            (*paquet)->telnet = t;
+            parse = 1;
+        }
+    }else{
+        printf("ccc\n");
+    }
+    return parse;
+}
+
+void displayPaquet(struct paquet** paquet){
+    if((*paquet)->ip->ip_p == IPPROTO_TCP){
+        numero++;
+        printf("%4i | ",numero);
+        if ((unsigned short int)ntohs((*paquet)->tcp->dest) == 23){
+            couleur(YELLOW);
+            printf("Serveur ");
+            couleur(DEFAULT);
+            if((*paquet)->telnet->size_telnet>0){
+                print_telnet(&(*paquet)->telnet);
+            }else{
+                print_tcp_apli(&(*paquet)->tcp);
+            }
+        }
+        if ((unsigned short int)ntohs((*paquet)->tcp->source) == 23){
+            couleur(CYAN);
+            printf("Client  ");
+            couleur(DEFAULT);
+            if((*paquet)->telnet->size_telnet>0){
+                print_telnet(&(*paquet)->telnet);
+            }else{
+                print_tcp_apli(&(*paquet)->tcp);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void destroyPaquet(struct paquet** paquet){
+    destroyTelnet(&(*paquet)->telnet);
+    free(*paquet);
+}
+
+void paquet(u_char *args, const struct pcap_pkthdr *hdr, const u_char *truepacket){
+    struct paquet* paquet = malloc(sizeof(struct paquet));
+    ERROR_NULL_P(paquet, "parsePaquet : paquet : paquet.c");
+    if(paquet != NULL){
+        int error = parsePaquet(truepacket, &paquet);
+        ERROR_SYS_P(error, "parsePaquet : paquet : paquet.c");
+        if(error == 1){
+            displayPaquet(&paquet);
+        }
+        destroyPaquet(&paquet);
+    }
+}
---
+++ src/paquet.h
@@ -0,0 +1,9 @@
+#ifndef PAQUET_H
+#define PAQUET_H
+
+#include "tcp.h"
+#include "telnet.h"
+
+void paquet(u_char *args, const struct pcap_pkthdr *hdr, const u_char *truepacket);
+
+#endif
