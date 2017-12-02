3b53f4af4c4cd4be3f03b53c3d2e6401eb37a044
PierrickJACQUETTE <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-12-02 14:25:52 +0100

    prise en compte des options de sb pour 28 40 42 44

--- src/telnet_sb.c
+++ src/telnet_sb.c
@@ -75,6 +75,76 @@ void checkZeroUntilThree(unsigned char** message, int* i, char* zero, char* one,
     }
 }
 
+void checkZeroUntilEight(unsigned char** message, int* i, char* zero, char* one, char* two, char* three, char* four, char* five, char* six, char* seven, char* eight){
+    checkZeroUntilThree(message, i, zero, one, two, three);
+    if((*message)[*i] == 4){
+        print(four, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 5){
+        print(five, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 6){
+        print(six, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 7){
+        print(seven, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 8){
+        print(eight, (*message)[*i]);
+        *i = *i +1;
+    }
+}
+
+void checkEightUntilEleven(unsigned char** message, int* i, char* eight, char* nine, char* ten, char* eleven){
+	if((*message)[*i] == 8){
+        print(eight, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 9){
+        print(nine, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 10){
+        print(ten, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 11){
+        print(eleven, (*message)[*i]);
+        *i = *i +1;
+    }
+}
+
+void check0Until4And8Until11(unsigned char** message, int* i, char* zero, char* one, char* two, char* three, char* four, char* eight, char* nine, char* ten, char* eleven){
+    checkZeroUntilThree(message, i, zero, one, two, three);
+    if((*message)[*i] == 4){
+        print(four, (*message)[*i]);
+        *i = *i +1;
+    }
+    checkEightUntilEleven(message, i, eight, nine, ten, eleven);
+}
+
+void checkOneUntilSeven(unsigned char** message, int* i, char* one, char* two, char* three, char* four, char* five, char* six, char* seven){
+    if((*message)[*i] == 1){
+        print(one, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 2){
+        print(two, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 3){
+        print(three, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 4){
+        print(four, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 5){
+        print(five, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 6){
+        print(six, (*message)[*i]);
+        *i = *i +1;
+    }else if((*message)[*i] == 7){
+        print(seven, (*message)[*i]);
+        *i = *i +1;
+    }
+}
+
 void bmReason(unsigned char** message, int* i){
     if((*message)[*i] == 1){
         print("bad-choice", (*message)[*i]);
@@ -147,13 +217,35 @@ void newEnv(unsigned char** message, int* i){
     checkZeroUntilTwo(message, i, "is", "send", "info");
     checkZeroUntilThree(message, i, "var", "value", "esc", "uservar");
     while((*message)[*i] != 255){
-        if((*message)[*i] == 3 || (*message)[*i] == 2 || (*message)[*i] == 1 || (*message)[*i] == 0){
+        if((*message)[*i] <= 3 && (*message)[*i] >= 0){
             checkZeroUntilThree(message, i, "var", "value", "esc", "uservar");
         }else{
             printf("%c", (*message)[*i]);
             *i = *i +1;
         }
     }
+}
+
+void encrypt(unsigned char** message, int* i){
+	checkZeroUntilEight(message, i, "is", "support", "reply", "start", "end", "request-start", "request-end", "enc_keyid", "dec_keyid");
+    while((*message)[*i] != 255){
+        if((*message)[*i] <= 11 && (*message)[*i] >= 0){
+			check0Until4And8Until11(message, i, "null", "des_cfb64", "des_ofb64", "des3_cfb64", "des3_ofb64", "cast5_40_cfb64", "cast5_40_ofb64", "cast128_cfb64", "cast128_ofb64");
+        }else{
+            printf("%d", (*message)[*i]);
+            *i = *i +1;
+        }
+    }
+}
+
+void comPort(unsigned char** message, int* i){
+	checkOneUntilSeven(message, i, "set-baudrate", "set-datasize", "set-parity", "set-stopsize", "set-control", "notify-linestate", "notify-modemstate");
+	checkEightUntilEleven(message, i, "flowcontrol-suspend", "flowcontrol-resume", "set-linestate-mask", "set-modemstate-stack");
+    if((*message)[*i] == 12){
+        print("purge-data", (*message)[*i]);
+        *i = *i +1;
+    }
+    read255(message, i, 1);
 }
 
 
@@ -193,6 +285,7 @@ void print_sb(unsigned char** message, int* i, int* option){
         case 23:
             read255(message, i, 0);
             break;
+        case 26:
         case 27:
             read32(message, i);
             break;
@@ -220,9 +313,24 @@ void print_sb(unsigned char** message, int* i, int* option){
             checkZeroUntilThree(message, i, "is", "send", "reply", "name");
             read255(message, i, 0);
             break;
+        case 38:
+        	encrypt(message, i);
+        	break;
         case 39:
             newEnv(message, i);
             break;
+        case 40:
+        	checkZeroUntilEight(message, i, "associate", "connect", "device-type", "functions", "is", "reason", "reject", "request", "send");
+           	checkZeroUntilEight(message, i, "associate", "connect", "device-type", "functions", "is", "reason", "reject", "request", "send");
+           	read255(message, i, 1);
+        	break;
+        case 42:
+        	checkOneUntilSeven(message, i, "request", "accept", "reject", "ttable-is", "ttable-rejected", "ttable-ack", "ttable-nack");
+        	read255(message, i, 0);
+        	break;
+        case 44:
+        	comPort(message, i);
+        	break;
         default:
             break;
     }
